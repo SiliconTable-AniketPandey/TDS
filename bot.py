e1c0f2186ecc686b0ef660569876c47a15c4e60c
Aniket Pandey <50284597+SiliconTable-AniketPandey@users.noreply.github.com>
2026-08-12 16:19:48 +0530

    Update LOG_URL to use raw GitHub link

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 # --- fill these in with your own values ---
 TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 AIPIPE_TOKEN = os.environ["AIPIPE_TOKEN"]
-LOG_URL = "https://github.com/SiliconTable-AniketPandey/TDS/blob/main/Project%201/run.jsonl"
+LOG_URL = "https://raw.githubusercontent.com/SiliconTable-AniketPandey/TDS/refs/heads/main/run.jsonl"
 # -------------------------------------------
 
 client = OpenAI(base_url="https://aipipe.org/openai/v1", api_key=AIPIPE_TOKEN)
